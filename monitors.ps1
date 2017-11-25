5cd968d22f45a9ae5c837e1cb571cb2215a012b7
James <anisotropics@gmail.com>
2017-11-26 11:30:53 +1300

    Build 01

---
+++ monitors.ps1
@@ -0,0 +1,22 @@
+$monitors = get-ciminstance -class win32_pnpentity | where-object service -eq monitor
+
+$moncount = 0
+
+foreach ($monitor in $monitors)
+{
+	$moncount++
+	$mid = $monitor.PNPDeviceID + "_0"
+	$minfo = get-ciminstance -class wmimonitorid -namespace root\wmi | Where-Object instancename -eq $mid
+
+	$name = $monitor.name
+	$manufacturer = ($minfo.ManufacturerName -notmatch 0 | foreach-object {[char]$_}) -join ""
+	$serial = ($minfo.SerialNumberID -notmatch 0 | foreach-object {[char]$_}) -join ""
+	$manufacturedate = "Week " + $minfo.WeekOfManufacture + " " + $minfo.YearOfManufacture
+	
+	write-host "Monitor: $moncount"
+	write-host "Name: $name"
+	write-host "Manufacturer: $manufacturer"
+	write-host "Serial: $serial"
+	write-host "Manufacture Date: $manufacturedate `n"
+}
+
