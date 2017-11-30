fea72565b33f0884a02fcc0aeed645a28ac90a1a
James <anisotropics@gmail.com>
2017-12-01 09:37:07 +1300

    ad get computers
    checks added
    win10 only with cim

--- monitors.ps1
+++ monitors.ps1
@@ -1,23 +1,42 @@
-$monitors = get-ciminstance -class win32_pnpentity | where-object service -eq monitor
+$windowscomputers = get-adcomputer -filter * -prop * | where operatingsystem -match "Windows (7|10|XP)" | select name, lastlogondate, operatingsystem | select -first 10
 
-# need to add client network test before cim call and db connect code
-$moncount = 0
+$today = get-date
 
-foreach ($monitor in $monitors)
+
+foreach ($computer in $windowscomputers)
 {
-	$moncount++
-	$mid = $monitor.PNPDeviceID + "_0"
-	$minfo = get-ciminstance -class wmimonitorid -namespace root\wmi | Where-Object instancename -eq $mid
+	if (test-connection -count 1 -comp $computer.name -ea silentlycontinue)
+    {   
+        try
+        {
+			$loggedinuser = (get-ciminstance -class cim_computersystem -comp $computer.name -ea silentlycontinue).username
+			write-host "Computer $computer.name - User $loggedinuser" -fore green
+			$monitors = get-ciminstance -class win32_pnpentity -comp $computer.name | where-object service -eq monitor
+			
+			# need to add client network test before cim call and db connect code
+			$moncount = 0
+			
+			foreach ($monitor in $monitors)
+			{
+				$moncount++
+				$mid = $monitor.PNPDeviceID + "_0"
+				$minfo = get-ciminstance -class wmimonitorid -namespace root\wmi | Where-Object instancename -eq $mid
+			
+				$name = $monitor.name
+				$manufacturer = ($minfo.ManufacturerName -notmatch 0 | foreach-object {[char]$_}) -join ""
+				$serial = ($minfo.SerialNumberID -notmatch 0 | foreach-object {[char]$_}) -join ""
+				$manufacturedate = "Week " + $minfo.WeekOfManufacture + " " + $minfo.YearOfManufacture
+				
+				write-host "Monitor: $moncount"
+				write-host "Name: $name"
+				write-host "Manufacturer: $manufacturer"
+				write-host "Serial: $serial"
+				write-host "Manufacture Date: $manufacturedate `n"
+				#test run on domain
+			}
+        }
+        catch [exception] { write-host $pc "refused wmi call from domain admin!" }
+	}
+} 
 
-	$name = $monitor.name
-	$manufacturer = ($minfo.ManufacturerName -notmatch 0 | foreach-object {[char]$_}) -join ""
-	$serial = ($minfo.SerialNumberID -notmatch 0 | foreach-object {[char]$_}) -join ""
-	$manufacturedate = "Week " + $minfo.WeekOfManufacture + " " + $minfo.YearOfManufacture
-	
-	write-host "Monitor: $moncount"
-	write-host "Name: $name"
-	write-host "Manufacturer: $manufacturer"
-	write-host "Serial: $serial"
-	write-host "Manufacture Date: $manufacturedate `n"
-}
 
