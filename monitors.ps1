9f9134d2e2a40f6a0df0f74b33e06e165eebfd25
James <anisotropics@gmail.com>
2017-12-04 23:34:42 +1300

    working workflow

--- monitors.ps1
+++ monitors.ps1
@@ -1,10 +1,5 @@
 $os = "Windows (7|10|XP)"
-$windowscomputers = get-adcomputer -filter * -prop * | where-object operatingsystem -match "Windows (7|10)" | select name, lastlogondate, operatingsystem | select -first 10
-
-$today = get-date
-$validdate = $today.AddMonths(-3)
-
-$cred = get-credential -username "james" -message "Password?"
+$windowscomputers = get-adcomputer -filter * -prop * | where-object operatingsystem -match "Windows (7|10)" | select name, lastlogondate, operatingsystem | select -first 20
 
 function convertto-char
 (	
@@ -19,43 +14,70 @@ function convertto-char
 	return $Output
 }
 
+workflow collectinfo
+{   
+    param
+	(
+    	[parameter(mandatory=$true, position=0)]
+    	[alias("comp")]$computers
 
-foreach ($computer in $windowscomputers)
-{
-	if (($computer.lastlogondate -gt $validdate) -and (test-connection -count 1 -comp $computer.name -ea silentlycontinue))
-    {   
-        try
-        {
-			$loggedinuser = invoke-command -comp $computer.name -cred $cred -script {(get-ciminstance -class cim_computersystem -ea silentlycontinue).username}
-			write-host "Computer $computer.name - User $loggedinuser" -fore green
-			$monitors = invoke-command -comp $computer.name -cred $cred -script {get-ciminstance -class win32_pnpentity -comp $computer.name | where-object service -eq monitor}
-			
-			# need to add client network test before cim call and db connect code
-			$moncount = 0
-			
-			foreach ($monitor in $monitors)
+	)
+
+	$today = get-date
+	$validdate = $today.AddMonths(-3)
+
+	$username = "james@pncc.govt.nz"
+	$password = get-content d:\james\aenigma | convertto-securestring
+	
+	$cred = [pscredential]::new($username, $password) 
+	write-output $cred | select *
+	
+	foreach -parallel ($computer in $computers)
+	{
+		Write-Output $computer.name
+		#new-item -path "d:/james/temp/$computer.name" -type File
+		if (($computer.lastlogondate -gt $validdate) -and (test-connection -count 1 -comp $computer.name -ea silentlycontinue))
+		{   
+			write-output "before try block $computer.lastlogondate"
+			try
 			{
-				$moncount++
-				$global:mid = $monitor.PNPDeviceID + "_0"
-				$minfo = invoke-command -comp $computer.name -cred $cred -script {get-ciminstance -class wmimonitorid -namespace root\wmi | Where-Object instancename -eq $($args[0])} -ArgumentList $mid
-			
-				$name = $monitor.name
-				#$manufacturer = ($minfo.ManufacturerName -notmatch 0 | foreach-object {[char]$_}) -join ""
-				$makemodel = convertto-char($minfo.UserFriendlyName)
-				$serial = ($minfo.SerialNumberID -notmatch 0 | foreach-object {[char]$_}) -join ""
-				$manufacturedate = "Week " + $minfo.WeekOfManufacture + " " + $minfo.YearOfManufacture
+				write-output "before log user $computer"
+				$computername = $computer.name
+				$loggedinuser = (inlinescript {invoke-command -comp $using:computername -cred $using:cred -script {(get-ciminstance -class cim_computersystem -ea silentlycontinue).username}})
+				write-output "Computer $computer.name - User $loggedinuser"
+				write-output "after log user"
+				$monitors = (inlinescript {invoke-command -comp $using:computer.name -cred $using:cred -script {get-ciminstance -class win32_pnpentity | where-object service -eq monitor}})
 				
-				write-host "Monitor: $moncount"
-				write-host "Name: $name"
-				#write-host "Manufacturer: $manufacturer"
-				write-host "Make/Model: "$ufn
-				write-host "Serial: $serial"
-				write-host "Manufacture Date: $manufacturedate `n"
-				#test run on domain
+				# need to add client network test before cim call and db connect code
+				$moncount = 0
+				
+				foreach ($monitor in $monitors)
+				{
+					$moncount++
+					$monitorid = $monitor.PNPDeviceID + "_0"
+					$monitorinfo = (inlinescript {invoke-command -comp $using:computer.name -cred $using:cred -script {get-ciminstance -class wmimonitorid -namespace root\wmi | Where-Object instancename -eq $($args[0])} -ArgumentList $monitorid})
+				
+					$name = $monitor.name
+					#$manufacturer = ($minfo.ManufacturerName -notmatch 0 | foreach-object {[char]$_}) -join ""
+					$makemodel = (convertto-char($monitorinfo.UserFriendlyName)).split(" ")
+					$make = $makemodel[0]
+					$model = $makemodel[1]
+					$serial = ($monitorinfo.SerialNumberID -notmatch 0 | foreach-object {[char]$_}) -join ""
+					$manufacturedate = "Week " + $monitorinfo.WeekOfManufacture + " " + $monitorinfo.YearOfManufacture
+					
+					write-output "Monitor: $moncount"
+					write-output "Name: $name"
+					#write-host "Manufacturer: $manufacturer"
+					write-output "Make: "$make
+					write-output "Model: "$model
+					write-output "Serial: $serial"
+					write-output "Manufacture Date: $manufacturedate `n"
+					#test run on domain
+				}
 			}
-        }
-        catch [exception] { write-host $computer.name "refused cim call from domain admin!" }
-	}
-} 
-
+			catch [exception] { write-output $computer.name "refused cim call from domain admin!" }
+			$monitors = $null
+		}
+	} 
+}
 
