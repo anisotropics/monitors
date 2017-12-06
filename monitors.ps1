0ee5830ee9f5304d8aa94c5036360f1fb4c79c9a
James <anisotropics@gmail.com>
2017-12-07 12:34:19 +1300

    database inserts
    with no query for exist

--- monitors.ps1
+++ monitors.ps1
@@ -1,5 +1,5 @@
-$os = "Windows (7|10|XP)"
-$windowscomputers = get-adcomputer -filter * -prop * | where-object operatingsystem -match "Windows (7|10)" | select name, lastlogondate, operatingsystem | select -first 50
+#$os = "Windows (7|10|XP)"
+$windowscomputers = get-adcomputer -filter * -prop * | where-object operatingsystem -match "Windows (7|10)" | select-object name, lastlogondate, operatingsystem | select-object -first 20
 
 function global:convertto-char 
 (	
@@ -12,6 +12,42 @@ function global:convertto-char
 		$output += [char]$char -join ""
 	}
 	return $output
+}
+
+class collectedcomputer
+{
+	[guid]$guid
+	[string]$name
+	[string]$dateinstalled
+	[string]$lastloggedonuser
+	[string]$lastlogonts
+
+	collectedcomputer($guid, $name, $dateinstalled, $lastloggedonuser, $lastlogonts)
+	{
+		$this.guid = $guid
+		$this.name = $name
+		$this.dateinstalled = $dateinstalled
+		$this.lastloggedonuser = $lastloggedonuser
+		$this.lastlogonts = $lastlogonts
+	}
+}
+
+class collectedmonitor
+{
+	[string]$make
+	[string]$model
+	[string]$serialnumber
+	[string]$manufactureweek
+	[string]$manufactureyear
+
+	collectedmonitor($make, $model, $serialnumber, $manufactureweek, $manufactureyear)
+	{
+		$this.make = $make
+		$this.model = $model
+		$this.serialnumber = $serialnumber
+		$this.manufactureweek = $manufactureweek
+		$this.manufactureyear = $manufactureyear
+	}
 }
 
 workflow collectinfo
@@ -32,7 +68,39 @@ workflow collectinfo
 	$password = get-content d:\james\aenigma | convertto-securestring
 	
 	$cred = [pscredential]::new($username, $password) 
-	write-output $cred | select *
+
+	<#------------------
+		set up database insert\update objects  
+	#>
+	function global:mailboxtodb
+	{
+		foreach ($mailbox in get-mailbox)
+		{
+			$mailstats = get-mailboxstatistics -Identity $mailbox.userprincipalname
+			
+			$date = (get-date -Format "yyyy-MM-dd HH:mm:ss").tostring()
+			$guid = $mailbox.exchangeguid
+			$upn = $mailbox.UserPrincipalName
+			$email = $mailbox.WindowsEmailAddress
+			$size = $mailstats.TotalItemSize
+			$itemcount = $mailstats.ItemCount
+			$lastlogon = $mailstats.LastLoggedOnUserAccount
+			$name = $mailbox.displayname
+			$class = $mailstats.ObjectClass
+			$lastlogontimestamp = (get-date $mailstats.LastLogonTime -format "yyyy-MM-dd HH:mm:ss").tostring()
+	
+			write-host $date
+			write-host $lastlogontimestamp
+	
+			$mailinfo = new-object -type mailboxinfo $guid, $name, $email, $size, $date, $lastlogon, $itemcount, $upn, $class, $lastlogontimestamp
+			write-objecttosql -InputObject $mailinfo -Server sqldev3.pncc.govt.nz -Database exchangeinfo -TableName mailboxstats
+		}
+	
+		return $mailinfo
+	}
+
+	#-------------------
+
 	
 	foreach -parallel ($computer in $computers)
 	{
@@ -40,22 +108,28 @@ workflow collectinfo
 		#new-item -path "d:/james/temp/$computer.name" -type File
 		if (($computer.lastlogondate -gt $validdate) -and (test-connection -count 1 -comp $computer.name -ea silentlycontinue))
 		{   
+			$computername = $computer.name
+
 			try
 			{
-				$computername = $computer.name
 				$loggedinuser = inlinescript {invoke-command -computername $using:computername -credential $using:cred -scriptblock {(get-ciminstance -class cim_computersystem -ea silentlycontinue).username}}
 				write-output "Computer $computer.name - User $loggedinuser"
 				#write-output "after log user"
 				$monitors = inlinescript {invoke-command -comp $using:computer.name -credential $using:cred -scriptblock {get-ciminstance -class win32_pnpentity | where-object service -eq monitor}}
-				
-				# need to add client network test before cim call and db connect code
+				$computerobject = new-object -type collectedcomputer $guid, $name, $email, $size, $date, $lastlogon, $itemcount, $upn, $class, $lastlogontimestamp
+
+			}
+			catch [exception] { write-output $computer.name "that didn't work!" $error }
+
+			try
+			{
 				$moncount = 0
-				
+		
 				foreach ($monitor in $monitors)
 				{
 					$moncount++
 					$monitorid = $monitor.PNPDeviceID + "_0"
-					$monitorinfo = inlinescript {invoke-command -computername $using:computername -credential $using:cred -scriptblock {get-ciminstance -class wmimonitorid -namespace root\wmi | Where-Object instancename -eq $($args[0])} -ArgumentList $using:monitorid}
+					$monitorinfo = inlinescript {invoke-command -computername $using:computername -credential $using:cred -scriptblock {get-ciminstance -class wmimonitorid -namespace root\wmi | where-object instancename -eq $($args[0])} -argumentList $using:monitorid}
                 
 					$name = $monitor.name
 					#$manufacturer = ($minfo.ManufacturerName -notmatch 0 | foreach-object {[char]$_}) -join ""
@@ -65,11 +139,11 @@ workflow collectinfo
 					$make = $makemodel[0]
 					$model = $makemodel[1]
 					$serial = global:convertto-char($monitorinfo.SerialNumberID)
-					$manufacturedate = "Week " + $monitorinfo.WeekOfManufacture + " " + $monitorinfo.YearOfManufacture
+					$manufactureweek = $monitorinfo.WeekOfManufacture
+					$manufactureyear = $monitorinfo.YearOfManufacture
 					
 					write-output "Monitor: $moncount"
 				    write-output "Name: $name"
-					#write-output "Manufacturer: $manufacturer"
 					write-output "Make: $make"
 					write-output "Model: $model"
 					write-output "Serial: $serial"
@@ -77,9 +151,10 @@ workflow collectinfo
 					#test run on domain
 				}
 			}
-			catch [exception] { write-output $computer.name "refused cim call from domain admin!" }
+			catch [exception] { write-output $computer.name "that didn't work!" $error }
 			$monitors = $null
 		}
 	} 
 }
 
+collectinfo $windowscomputers
